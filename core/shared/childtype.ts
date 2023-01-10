6ad346235e3195ca5e99e5d162b224a47cfeaf49
dinhlongnguyen <dinhlongviolin1@gmail.com>
2023-01-11 00:18:00 +0700

    Feature/init taipy studio (#124)
    * init taipy-studio
    
    * remove quickstart
    
    * add github actions
    
    * update github actions

--- core/shared/childtype.ts
+++
@@ -1,8 +0,0 @@
-import { DataNode, Pipeline, Scenario, Task } from "./names";
-
-const childType: Record<string, string> = {
-  [Task]: DataNode,
-  [Pipeline]: Task,
-  [Scenario]: Pipeline,
-};
-export const getChildType = (nodeType: string) => childType[nodeType] || "";
